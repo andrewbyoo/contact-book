09652fd13bfa1cd7ec7802e1525c5c4b37f0201d
Andrew Yoo <andrewbyoo@gmail.com>
2022-03-29 14:22:38 -0400

    Remotes about route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :contacts
-  # get 'home/index'
-  get 'home/about'
   authenticated :user do
     root to: 'contacts#index', as: :authenticated_root
   end
